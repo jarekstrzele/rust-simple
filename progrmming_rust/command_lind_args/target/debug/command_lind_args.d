6c9efd04ef70d117b8d8cd3fa123ba790897b4f8
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-20 10:26:17 +0200

    Add command line args project

---
+++ progrmming_rust/command_lind_args/target/debug/command_lind_args.d
@@ -0,0 +1,1 @@
+/home/jarek/Desktop/Prog/rust-simple/progrmming_rust/command_lind_args/target/debug/command_lind_args: /home/jarek/Desktop/Prog/rust-simple/progrmming_rust/command_lind_args/src/main.rs
