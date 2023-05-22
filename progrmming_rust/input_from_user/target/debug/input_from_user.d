1cf57f865a5745a5fa1bf914a8cda626a81479e6
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-22 09:34:02 +0200

    Add input user project

---
+++ progrmming_rust/input_from_user/target/debug/input_from_user.d
@@ -0,0 +1,1 @@
+/home/jarek/Desktop/Prog/rust-simple/progrmming_rust/input_from_user/target/debug/input_from_user: /home/jarek/Desktop/Prog/rust-simple/progrmming_rust/input_from_user/src/main.rs
---
+++ progrmming_rust/patt_match/target/debug/deps/patt_match-4e8761bcc91c5356.d
@@ -0,0 +1,5 @@
+/home/jarek/Desktop/Prog/rust-simple/progrmming_rust/patt_match/target/debug/deps/patt_match-4e8761bcc91c5356: src/main.rs
+
+/home/jarek/Desktop/Prog/rust-simple/progrmming_rust/patt_match/target/debug/deps/patt_match-4e8761bcc91c5356.d: src/main.rs
+
+src/main.rs:
