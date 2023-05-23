a77f5a1d5a8dd39e2832e3fd818d8707087d0e10
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-23 17:24:14 +0200

    Add rust test project

---
+++ progrmming_rust/test_in_rust/target/debug/deps/test_in_rust-afae6975ee09b11d.d
@@ -0,0 +1,5 @@
+/home/jarek/Desktop/Prog/rust-simple/progrmming_rust/test_in_rust/target/debug/deps/test_in_rust-afae6975ee09b11d: src/main.rs
+
+/home/jarek/Desktop/Prog/rust-simple/progrmming_rust/test_in_rust/target/debug/deps/test_in_rust-afae6975ee09b11d.d: src/main.rs
+
+src/main.rs:
