82f2af4e0141f27c0a6f015bcd04bc7388a36c35
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-25 17:38:36 +0200

    Add map combinator project

---
+++ rust_beginners/map_combinator/target/debug/deps/map_combinator-abb8be8a02f5b8ac.d
@@ -0,0 +1,5 @@
+/home/jarek/Desktop/Prog/rust-simple/rust_beginners/map_combinator/target/debug/deps/map_combinator-abb8be8a02f5b8ac.rmeta: src/main.rs
+
+/home/jarek/Desktop/Prog/rust-simple/rust_beginners/map_combinator/target/debug/deps/map_combinator-abb8be8a02f5b8ac.d: src/main.rs
+
+src/main.rs:
