313185b8540132f0a652ce91621f6917d0e5734a
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-25 21:35:01 +0200

    Add test project

---
+++ rust_beginners/test_rust/target/debug/deps/test_rust-203ff4a6dd977685.d
@@ -0,0 +1,5 @@
+/home/jarek/Desktop/Prog/rust-simple/rust_beginners/test_rust/target/debug/deps/test_rust-203ff4a6dd977685: src/main.rs
+
+/home/jarek/Desktop/Prog/rust-simple/rust_beginners/test_rust/target/debug/deps/test_rust-203ff4a6dd977685.d: src/main.rs
+
+src/main.rs:
