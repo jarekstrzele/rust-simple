1b6e2e3ed464e7344ffe7eae02aaa37749037d73
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-31 11:24:56 +0200

    Add range rust project

---
+++ rust_beginners/range_rust/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let range1 = 1..=3 ; // 1,2,3
+    
+    let rang2 = 1..3 ; // 1,2
+
+    for num in 1..4{
+        println!("{}", num) ;
+    }
+
+    for ch in 'a'..='f'{
+        println!("{}", ch) ;
+    }
+}
---
+++ rust_beginners/range_rust/target/debug/range_rust.d
@@ -0,0 +1,1 @@
+/home/jarek/Desktop/Prog/rust-simple/rust_beginners/range_rust/target/debug/range_rust: /home/jarek/Desktop/Prog/rust-simple/rust_beginners/range_rust/src/main.rs
