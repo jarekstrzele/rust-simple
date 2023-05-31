6308e2048c7eb645683ad22cfacff011ed2d9747
jarek <jaroslaw.strzelecki@protonmail.com>
2023-05-31 11:20:15 +0200

    Add simple iterator on vec

---
+++ rust_beginners/iterators_rust/target/debug/deps/iterators_rust-f2c5ca61a4fcb57d.d
@@ -0,0 +1,5 @@
+/home/jarek/Desktop/Prog/rust-simple/rust_beginners/iterators_rust/target/debug/deps/iterators_rust-f2c5ca61a4fcb57d: src/main.rs
+
+/home/jarek/Desktop/Prog/rust-simple/rust_beginners/iterators_rust/target/debug/deps/iterators_rust-f2c5ca61a4fcb57d.d: src/main.rs
+
+src/main.rs:
