06c1bd5a9e59774df0a1cf1c908171f46efef503
jarek <jaroslaw.strzelecki@protonmail.com>
2023-06-17 19:45:27 +0200

    a

---
+++ customes/target/debug/deps/customes-87754056891b274b.d
@@ -0,0 +1,5 @@
+/home/jarek/Desktop/Prog/rust-simple/customes/target/debug/deps/customes-87754056891b274b.rmeta: src/main.rs
+
+/home/jarek/Desktop/Prog/rust-simple/customes/target/debug/deps/customes-87754056891b274b.d: src/main.rs
+
+src/main.rs:
--- proby/src/main.rs
+++ proby/src/main.rs
@@ -1,5 +1,21 @@
-use demo::print_from_lib ;
 
-fn main() {
-    print_from_lib();
+struct Customer {
+    age: Option<i32>,
+    email: String,
+}
+
+
+
+fn main(){
+    let mark = Customer {
+        age: Some(22), email:"mark@example.com".to_owned(),
+    } ;
+    let becky = Customer {
+        age: None, email: "becky@example.com".to_owned(),
+    } ;
+
+    match becky.age {
+        Some(age) => println!("customer is {:?} years old", age),
+        None => println!("customer age nor provided"),
+    };
 }
