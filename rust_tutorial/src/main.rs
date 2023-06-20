ea279f4afea98a91347d13f36bf0b8b20fd00ece
jarek <jaroslaw.strzelecki@protonmail.com>
2023-06-20 17:18:59 +0200

    Add some strings to rust_tutorial

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -8,10 +8,39 @@ use std::cmp::Ordering ;
 
 
 fn main() {
+
+
+
     
-    println!("Max u32: {}", u32::MAX) ;
-    println!("Max u64: {}", u64::MAX) ;
-    println!("Max u128: {}", u128::MAX) ;
-    println!("Max f32: {}", f32::MAX) ;
-    println!("Max f128: {}", f64::MAX) ;
+//     let random_num: i32 = rand::thread_rng().gen_range(1..6+1) ;
+//     println!("random number {:?}", random_num) ;
+   
+//     let st3: String = String::from("x r t b h k k a m c") ;
+//     let mut v1: Vec<char> = st3.chars().collect() ;
+//     println!("{:?}", st3.chars()) ;
+//     v1.sort();
+//     v1.dedup(); // delete duplicate
+//     for char in v1 {
+//         print!("{char}") ;
+//     }
+//     let st4: &str = "Random string" ;
+//     let mut st5: String = st4.to_string();
+//     println!("{}", st5);
+//     let byte_arr1: &[u8] = st5.as_bytes() ;
+//     println!("byte arr {:?}", byte_arr1) ;
+//     let st6: &str = &st5[0..6] ;
+//     println!("st6 {:?}", st6) ;
+//     println!("string length : {}", st6.len()) ;
+
+//     st5.clear();
+//     let st6: String = String::from("Just some ") ;
+//     let st7: String = String::from(" words") ;
+//     let st8: String = st6 + &st7 ;
+//     for char in st8.bytes(){
+//         print!("{}", char) ;
+//     }
+//     println!("\n{st8}") ;
+//    // println!("\n{st6}") ; // generates some errors
+
+
 }
---
+++ rust_tutorial/src/my_casting.rs
@@ -0,0 +1,8 @@
+fn my_cast(){
+
+    let int_max: u8 = u8::MAX ;
+    println!("{}", int_max) ;
+
+}
+
+
