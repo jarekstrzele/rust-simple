d685b3b80d861d65b6cd3302a970802de02de08e
jarek <jaroslaw.strzelecki@protonmail.com>
2023-07-12 11:44:14 +0200

    Add a new project closure_intro

---
+++ closures_intro/target/debug/closures_intro.d
@@ -0,0 +1,1 @@
+/home/jarek/Desktop/Prog/rust-simple/closures_intro/target/debug/closures_intro: /home/jarek/Desktop/Prog/rust-simple/closures_intro/src/main.rs
---
+++ closures_intro/target/debug/deps/closures_intro-143b6f7c840eab1e.d
@@ -0,0 +1,5 @@
+/home/jarek/Desktop/Prog/rust-simple/closures_intro/target/debug/deps/closures_intro-143b6f7c840eab1e: src/main.rs
+
+/home/jarek/Desktop/Prog/rust-simple/closures_intro/target/debug/deps/closures_intro-143b6f7c840eab1e.d: src/main.rs
+
+src/main.rs:
