c548f5e90106e087d27915139a5b8d3106e877fd
jarek <jaroslaw.strzelecki@protonmail.com>
2023-07-18 18:11:48 +0200

    x

--- closures_intro/src/main.rs
+++ closures_intro/src/main.rs
@@ -11,5 +11,34 @@ fn simulated_expensive_calculation(intensity: u32) -> u32{
 
 
 fn main() {
-    println!("Hello, world!");
+   let simulated_insensity = 10 ;
+   let simulated_random_number = 7 ;
+
+   generate_workout(simulated_insensity, simulated_random_number) ;
+
+
+}
+
+
+fn generate_workout(intensity: u32, random_number: u32){
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            simulated_expensive_calculation(intensity)
+    );
+    println!(
+        "Next, do {} situps!",
+        simulated_expensive_calculation(intensity)
+    );
+
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!") ;
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                simulated_expensive_calculation(intensity)
+            ) ;
+        }
+    }
 }
